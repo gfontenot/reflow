d1da3944f6a0dfe96c1df5b7c97c9756791fbc26
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-10-12 18:21:46 -0500

    Split strings at 80 characters
    This adds a simple `wrap` function that takes a string and returns a
    list of strings broken at 80 characters. It's super naive and will
    _totally_ break in the middle of a word.

--- Main.hs
+++ Main.hs
@@ -1,1 +1,12 @@
+import qualified Data.Text as T (strip, pack, unpack)
+
 main = putStrLn "It works!"
+
+wrap :: String -> [String]
+wrap = map trim . combine . splitAt 80
+
+combine :: (a, a) -> [a]
+combine (a, b) = [a, b]
+
+trim :: String -> String
+trim = T.unpack . T.strip . T.pack
