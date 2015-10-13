ddc776015fce7e58fbe172b54574a7bf75295a44
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-10-13 18:12:59 -0500

    Wrap lines based on word boundries
    This makes line wrapping a little smarter. Instead of hard-wrapping on
    80 (or however many) characters, we can wrap based on word boundries if
    adding another word to the line pushes it past 80 (or however many)
    characters.
    
    This also makes the line length arbitrary.

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,22 @@ import qualified Data.Text as T (strip, pack, unpack)
 
 main = putStrLn "It works!"
 
-wrap :: String -> [String]
-wrap = map trim . combine . splitAt 80
+wrap :: Int -> String -> [String]
+wrap n s = removeEmpty . map trim . foldl (splitWordsAt n) [] . words $ s
 
-combine :: (a, a) -> [a]
-combine (a, b) = [a, b]
+splitWordsAt :: Int -> [String] -> String -> [String]
+splitWordsAt _ [] x = [x]
+splitWordsAt n xs x
+  | lineIsTooLong n (unwords [last xs, x]) = xs ++ [x]
+  | otherwise = init xs ++ [(unwords [last xs, x])]
+
+lineIsTooLong :: Int -> String -> Bool
+lineIsTooLong n xs = case length xs `compare` n of
+  GT -> True
+  otherwise -> False
 
 trim :: String -> String
 trim = T.unpack . T.strip . T.pack
+
+removeEmpty :: [[a]] -> [[a]]
+removeEmpty = filter (not . null)
