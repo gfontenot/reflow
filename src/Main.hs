ecef7d90d4716a2f19f6fd338307528c684d58f6
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-10-13 19:02:47 -0500

    PR comments

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,16 @@
 main = putStrLn "It works!"
 
 wrap :: Int -> String -> [String]
-wrap n s = foldl (splitWordsAt n) [] . words $ s
+wrap n s = foldl (splitWordsAt n) [] $ words s
 
 splitWordsAt :: Int -> [String] -> String -> [String]
 splitWordsAt _ [] x = [x]
 splitWordsAt n xs x
-  | lineIsTooLong n (unwords [last xs, x]) = xs ++ [x]
-  | otherwise = init xs ++ [(unwords [last xs, x])]
+  | lineIsTooLong n (appendToLast xs x) = xs ++ [x]
+  | otherwise = init xs ++ [appendToLast xs x]
+
+appendToLast :: [String] -> String -> String
+appendToLast xs x = unwords [last xs, x]
 
 lineIsTooLong :: Int -> String -> Bool
 lineIsTooLong n xs = length xs > n
